f0e5fa44a5abe7618a21e7514132f1b326f9b3db
Iurii Ignatko <iignatko@gradle.com>
2023-02-14 23:05:27 +0200

    Initial commit

---
+++ src/main/java/com/github/welandaz/Main.java
@@ -0,0 +1,9 @@
+package com.github.welandaz;
+
+public final class Main {
+
+    public static void main(final String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
---
+++ src/test/java/com/github/welandaz/MainTest.java
@@ -0,0 +1,14 @@
+package com.github.welandaz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+final class MainTest {
+
+    @Test
+    void alwaysPasses() {
+        assertTrue(true);
+    }
+
+}
